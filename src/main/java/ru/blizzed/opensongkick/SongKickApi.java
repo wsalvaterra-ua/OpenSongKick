ffd23055e231ceab4868634cac7bdffd8250e7fe
BlizzedRu <me@blizzed.ru>
2017-12-13 01:23:23 +0300

    Removed useless IOExceptions in SongKickApi

--- src/main/java/ru/blizzed/opensongkick/SongKickApi.java
+++ src/main/java/ru/blizzed/opensongkick/SongKickApi.java
@@ -1,6 +1,8 @@
 package ru.blizzed.opensongkick;
 
-import ru.blizzed.opensongkick.methods.*;
+import ru.blizzed.opensongkick.methods.EventDetailsCaller;
+import ru.blizzed.opensongkick.methods.SimilarArtistsCaller;
+import ru.blizzed.opensongkick.methods.VenueDetailsCaller;
 import ru.blizzed.opensongkick.methods.calendar.*;
 import ru.blizzed.opensongkick.methods.gigography.ArtistGigography;
 import ru.blizzed.opensongkick.methods.gigography.ArtistGigographyCaller;
@@ -8,8 +10,6 @@ import ru.blizzed.opensongkick.methods.gigography.UserGigography;
 import ru.blizzed.opensongkick.methods.gigography.UserGigographyCaller;
 import ru.blizzed.opensongkick.methods.search.*;
 import ru.blizzed.opensongkick.models.*;
-
-import java.io.IOException;
 
 public final class SongKickApi {
 
@@ -33,65 +33,65 @@ public final class SongKickApi {
     private SongKickApi() {
     }
 
-    public static ArtistSearch artistSearch() throws IOException {
+    public static ArtistSearch artistSearch() {
         if (artistSearch == null) artistSearch = OpenSongKickContext.getInstance().createService(ArtistSearchCaller.class);
         return new ArtistSearch(artistSearch);
     }
 
-    public static EventSearch eventSearch() throws IOException {
+    public static EventSearch eventSearch() {
         if (eventSearchCaller == null) eventSearchCaller = OpenSongKickContext.getInstance().createService(EventSearchCaller.class);
         return new EventSearch(eventSearchCaller);
     }
 
-    public static LocationSearch locationSearch() throws IOException {
+    public static LocationSearch locationSearch() {
         if (locationSearchCaller == null) locationSearchCaller = OpenSongKickContext.getInstance().createService(LocationSearchCaller.class);
         return new LocationSearch(locationSearchCaller);
     }
 
-    public static VenueSearch venueSearch() throws IOException {
+    public static VenueSearch venueSearch() {
         if (venueSearchCaller == null) venueSearchCaller = OpenSongKickContext.getInstance().createService(VenueSearchCaller.class);
         return new VenueSearch(venueSearchCaller);
     }
 
 
-    public static ArtistCalendar artistCalendar() throws IOException {
+    public static ArtistCalendar artistCalendar() {
         if (artistCalendarCaller == null) artistCalendarCaller = OpenSongKickContext.getInstance().createService(ArtistCalendarCaller.class);
         return new ArtistCalendar(artistCalendarCaller);
     }
 
-    public static VenueCalendar venueCalendar() throws IOException {
+    public static VenueCalendar venueCalendar() {
         if (venueCalendarCaller == null) venueCalendarCaller = OpenSongKickContext.getInstance().createService(VenueCalendarCaller.class);
         return new VenueCalendar(venueCalendarCaller);
     }
 
-    public static MetroAreaCalendar metroAreaCalendar() throws IOException {
+    public static MetroAreaCalendar metroAreaCalendar() {
         if (metroAreaCalendarCaller == null) metroAreaCalendarCaller = OpenSongKickContext.getInstance().createService(MetroAreaCalendarCaller.class);
         return new MetroAreaCalendar(metroAreaCalendarCaller);
     }
 
 
-    public static ArtistGigography artistGigography() throws IOException {
+    public static ArtistGigography artistGigography() {
         if (artistGigographyCaller == null) artistGigographyCaller = OpenSongKickContext.getInstance().createService(ArtistGigographyCaller.class);
         return new ArtistGigography(artistGigographyCaller);
     }
 
-    public static UserGigography userGigography() throws IOException {
+    public static UserGigography userGigography() {
         if (userGigographyCaller == null) userGigographyCaller = OpenSongKickContext.getInstance().createService(UserGigographyCaller.class);
         return new UserGigography(userGigographyCaller);
     }
 
 
-    public static ApiCaller<Result<Event>> eventDetails(String eventId) throws IOException {
+    public static ApiCaller<Result<Event>> eventDetails(String eventId) {
         if (eventDetailsCaller == null) eventDetailsCaller = OpenSongKickContext.getInstance().createService(EventDetailsCaller.class);
         return new ApiCaller<>(eventDetailsCaller.byId(eventId));
     }
 
-    public static ApiCaller<Result<Venue>> venueDetails(String venueId) throws IOException {
+    public static ApiCaller<Result<Venue>> venueDetails(String venueId) {
         if (venueDetailsCaller == null) venueDetailsCaller = OpenSongKickContext.getInstance().createService(VenueDetailsCaller.class);
         return new ApiCaller<>(venueDetailsCaller.byId(venueId));
     }
 
-    public static ApiCaller<ResultsPage<Artist>> similarArtists(String artistId) throws IOException {
+    public static ApiCaller<ResultsPage<Artist>> similarArtists(String artistId) {
         if (similarArtistsCaller == null) similarArtistsCaller = OpenSongKickContext.getInstance().createService(SimilarArtistsCaller.class);
         return new ApiCaller<>(similarArtistsCaller.byId(artistId));
     }
